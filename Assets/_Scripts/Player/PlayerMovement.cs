78f71d44240e714fc044c67774d4d729ba1b2a36
Ramin Amiri <zeuspsn.55555@gmail.com>
2023-04-30 15:02:50 -0400

    line draw movement

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -8,7 +8,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform planetTransform;
     [SerializeField] private float moveCooldownMin;
     [SerializeField] private float moveCooldownMax;
-    [SerializeField] private float moveLineMaxLength;
+    [SerializeField] private float movePowerMin;
+    [SerializeField] private float movePowerMax;
+
+    [Tooltip("How much percent of the screen the move line can take up.")]
+    [SerializeField] [Range(0.15f, 0.5f)] private float moveLineMaxLength;
+
     [SerializeField] private LineRenderer lineRenderer;
 
     private Camera mainCamera;
@@ -17,92 +22,90 @@ public class PlayerMovement : MonoBehaviour
     // Movement variables.
     private bool isMoving = false;
     private bool isSettingMoveLine = false;
-
-    // Move cooldown variables.
     private float moveCooldown;
-    private float moveCooldownTimer;
-    private float moveCooldownLineDistanceRange; // Number 0-1, 0 being the min distance and 1 being the max distance. 
+    [SerializeField] private float moveCooldownTimer;
+    private float movePower;
 
-    private float moveLineDistance;
+    // Move line variables.
+    private float moveLineEffectiveDistanceScreenSpace;
+    private float moveLineDistanceRange; // Number 0-1, 0 being the min distance and 1 being the max distance. Can go above 1.
+    private float moveLineLength;
 
     // Mouse position variables.
-    private float distanceBetweenMouseEvents;
+    Vector2 dirTowardsMousePos;
+    float distBetweenMouseEventsClamped;
     private Vector2 mousePos;
-    private Vector2 lastValidMousePos; // Last valid mouse position for move line placement. Going above max line distance will stop updating this value.
     private Vector2 lastMouseClickPos;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         mainCamera = Camera.main;
+        moveLineEffectiveDistanceScreenSpace = CalculateDiagonalScreenSize() * moveLineMaxLength;
     }
 
     private void Update()
     {
         if (isSettingMoveLine)
         {
-            mousePos = Mouse.current.position.ReadValue();
-
-            //Debug.Log("DISTANCE: " + Vector2.Distance(lineRenderer.GetPosition(0), mousePos));
-            //Debug.Log("MAX LINE DISTANCE: " + maxLineDistance);
-
-            if (Vector2.Distance(lineRenderer.GetPosition(0), mousePos) > moveLineMaxLength)
-            {
-                lineRenderer.SetPosition(1, mainCamera.ScreenToWorldPoint(lastValidMousePos));
-            }
-            else
-            {
-                lineRenderer.SetPosition(1, mainCamera.ScreenToWorldPoint(mousePos));
-                lastValidMousePos = mousePos;
-            }
-
-            lineRenderer.SetPosition(1, new Vector3(lineRenderer.GetPosition(1).x, lineRenderer.GetPosition(1).y, 0));
+            CalculateMoveLine();
         }
     }
 
     private void OnLMB(InputValue value)
     {
+        if (isMoving)
+            return;
+
         // Get the mouse position.
         mousePos = Mouse.current.position.ReadValue();
 
         if (value.isPressed)       // LMB Pressed
         {
+            // Set the last mouse click position. This is used to calculate the move line length.
             lastMouseClickPos = mousePos;
 
+            // Set the start position of the move line.
             lineRenderer.SetPosition(0, mainCamera.ScreenToWorldPoint(lastMouseClickPos));
             lineRenderer.SetPosition(0, new Vector3(lineRenderer.GetPosition(0).x, lineRenderer.GetPosition(0).y, 0));
 
+            // The player has clicked a button to begin setting the move line.
             isSettingMoveLine = true;
         }
-        else if (!value.isPressed) // LMB Released
+        else if (!value.isPressed && isSettingMoveLine == true) // LMB Released
         {
-            // Get the distance between the mouse events.
-            distanceBetweenMouseEvents = Vector2.Distance(mousePos, lastMouseClickPos);
-
-            // Max distance of the mouse events is half of the diagonal screen size.
-            moveCooldownLineDistanceRange = distanceBetweenMouseEvents / (CalculateDiagonalScreenSize() * 0.5f);
-
             // Move cooldown is the max distance multiplied by the difference between the max and min move cooldowns.
-            moveCooldown = Mathf.Lerp(moveCooldownMin, moveCooldownMax, moveCooldownLineDistanceRange);
-            moveLineDistance = Mathf.Lerp(0, moveLineMaxLength, moveCooldownLineDistanceRange);
+            moveCooldown = Mathf.Lerp(moveCooldownMin, moveCooldownMax, moveLineDistanceRange);
 
-            //Debug.Log("Mouse Events Distance: " + distanceBetweenMouseEvents);
-            //Debug.Log("Move CD: " + moveCooldown);
+            // Move power is the max distance multiplied by the difference between the max and min move powers.
+            movePower = Mathf.Lerp(movePowerMin, movePowerMax, moveLineDistanceRange);
 
-            lineRenderer.SetPosition(0, mainCamera.ScreenToWorldPoint(lastMouseClickPos));
-            lineRenderer.SetPosition(1, mainCamera.ScreenToWorldPoint(mousePos));
+            // Reset the move line.
+            lineRenderer.SetPosition(0, Vector2.zero);
+            lineRenderer.SetPosition(1, Vector2.zero);
 
-            // Set the Z position of the line renderer to 0.
-            lineRenderer.SetPosition(0, new Vector3(lineRenderer.GetPosition(0).x, lineRenderer.GetPosition(0).y, 0));
-            lineRenderer.SetPosition(1, new Vector3(lineRenderer.GetPosition(1).x, lineRenderer.GetPosition(1).y, 0));
-
+            // The player has released the button and is no longer setting the move line.
             isSettingMoveLine = false;
+
+            isMoving = true;
+
+            rb.AddForce(dirTowardsMousePos * movePower, ForceMode2D.Impulse);
+
+            StartCoroutine(CountdownMoveCooldown());
         }
     }
 
-    private IEnumerator AddConstantForceOverTime(Vector2 force)
+    private IEnumerator CountdownMoveCooldown()
     {
-        rb.AddForce(force);
+        moveCooldownTimer = moveCooldown;
+
+        while (moveCooldownTimer > 0)
+        {
+            moveCooldownTimer -= Time.deltaTime;
+            yield return null;
+        }
+
+        isMoving = false;
 
         yield return null;
     }
@@ -110,6 +113,40 @@ public class PlayerMovement : MonoBehaviour
     public void AddPlanetGravity(Vector2 force, Vector2 direction)
     {
         rb.AddForce(force);
+    }
+
+    private void CalculateMoveLine()
+    {
+        // Get the mouse position.
+        mousePos = Mouse.current.position.ReadValue();
+
+        // Get the direction towards the mouse position from the last mouse click position.
+        dirTowardsMousePos = (mousePos - lastMouseClickPos).normalized;
+
+        // Get the distance between the last mouse click position and the mouse position.
+        float distBetweenMouseEvents = Vector2.Distance(lastMouseClickPos, mousePos);
+
+        // Clamp the distance between the last mouse click position and the mouse position to the max move line distance.
+        distBetweenMouseEventsClamped = Mathf.Clamp(distBetweenMouseEvents, 0, moveLineEffectiveDistanceScreenSpace);
+
+        // Get the current move line distance range (0 = min, 1 = max).
+        moveLineDistanceRange = distBetweenMouseEventsClamped / moveLineEffectiveDistanceScreenSpace;
+
+        // Lerp the move line length between 0 and the max move line length based on the current move line distance range.
+        moveLineLength = Mathf.Lerp(0, moveLineEffectiveDistanceScreenSpace, moveLineDistanceRange);
+
+        // Set the end position of the move line.
+        Vector2 endPos = lastMouseClickPos + (dirTowardsMousePos * distBetweenMouseEventsClamped);
+
+        // Set the start position of the move line.
+        lineRenderer.SetPosition(0, mainCamera.ScreenToWorldPoint(lastMouseClickPos));
+        lineRenderer.SetPosition(0, new Vector3(lineRenderer.GetPosition(0).x, lineRenderer.GetPosition(0).y, 0));
+
+        // Set the end position of the move line in world space.
+        lineRenderer.SetPosition(1, mainCamera.ScreenToWorldPoint(endPos));
+        lineRenderer.SetPosition(1, new Vector3(lineRenderer.GetPosition(1).x, lineRenderer.GetPosition(1).y, 0));
+
+        //Debug.Log("move line length (screen-space): " + moveLineLength);
     }
 
     private float CalculateDiagonalScreenSize()
