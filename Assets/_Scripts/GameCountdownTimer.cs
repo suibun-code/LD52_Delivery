de97c3ad2cf87c96c78438862a3ba621510e0a5d
Ramin Amiri <zeuspsn.55555@gmail.com>
2023-05-01 19:39:07 -0400

    push2

---
+++ Assets/_Scripts/GameCountdownTimer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCountdownTimer : MonoBehaviour
+{
+    [SerializeField] private float countdownTime;
+
+    private float countdown;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
